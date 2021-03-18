c06c226151bc3c44db8b71deefb33c572f657226
Orlando Marin <orlandojermainemarin@gmail.com>
2021-03-17 17:03:45 -0700

    completed lab

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,11 @@
 // Code CoordinatesButton Component Here
+
+import React from 'react'
+
+export default class Clicker extends React.Component {
+    render() {
+      return (
+        <button onClick={(event) => this.props.onReceiveCoordinates([event.clientX, event.clientY])}>Click me for Mouse Coordinates!</button>
+      )
+    }
+    }
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,15 @@
 // Code DelayedButton Component Here
+
+import React from 'react' 
+
+export default class DelayedButton extends React.Component {
+    handleClick = (event) => {
+        event.persist()
+        return setTimeout(() => this.props.onDelayedClick(event), this.props.delay) 
+    }
+    render() {
+            return (
+                <button onClick={(event) => this.handleClick(event)}>Click Me!</button>
+        )
+    }
+}
